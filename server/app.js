16246ad2f02af3b5d57e2a637609f07c1fe76005
schennam714 <shreyas.chennamaraja@gmail.com>
2024-01-17 09:34:39 -0600

    fixing express problems

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+var express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
